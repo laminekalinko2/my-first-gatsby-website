19a261beeb360f333920ae10ee79ec4e9bb1b496
Lamine Diallo <laminekalinko2@mac.home>
2024-11-16 08:16:48 -0500

    Add static image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `My First Gatsby website`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
